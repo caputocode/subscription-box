768a9ff524a106c6afa864fdd5ec2d0708344928
Heather Caputo <caputoheather@ymail.com>
2019-01-30 09:55:57 +0000

    added accounts app to set up User login and Registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
-from .views import register
+from .views import register, login, login_required
 
 urlpatterns = [
     url(r'^register/$', register, name='register'),
+    url(r'^login/$', login, name='login'),
+    url(r'^login_required/$', login_required(), name='login_required'),
+
     ]
