66cdb5ff8b75fe354bd97b5ff286c2abc0d09a34
Heather Caputo <caputoheather@ymail.com>
2019-01-30 09:37:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<!--install Django-->
+sudo pip3 install django==1.11.15
+<!--create project in root -->
+django-admin startproject subscription_box .
+<!--create boxes app -->
+django-admin startapp boxes
---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.core.urlresolvers import reverse
+from .forms import UserLoginForm, UserRegistrationForm
+from django.template.context_processors import csrf
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def register(request):
+    """A view that manages the registration form"""
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+
+            user = auth.authenticate(request.POST.get('email'),
+                                     password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "Registration successful!")
+                return redirect(reverse('index'))
+
+            else:
+                messages.error(request, "unable to log you in at this time!")
+    else:
+        user_form = UserRegistrationForm()
+
+    args = {'user_form': user_form}
+    return render(request, 'register.html', args)
